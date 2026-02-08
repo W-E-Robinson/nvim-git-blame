3bb1f6efaa0b3454551a07cd925ea94d5739cd08
Will Robinson <william.ellis.robinson@gmail.com>
2026-02-08 23:15:30 +0000

    feat: chosen lines are now highligthing
    need to start accounting for movement, duplication to hashes window, and
    closing of either one (hashe windows should close on main close)

--- lua/functionality/files_commit_hashes.lua
+++ lua/functionality/files_commit_hashes.lua
@@ -32,7 +32,6 @@ local function set_hashes_win_and_buf_properties(hashes_win, hashes_buf)
 
     vim.api.nvim_win_set_option(hashes_win, "number", false)
     vim.api.nvim_win_set_option(hashes_win, "relativenumber", false)
-    vim.api.nvim_win_set_option(hashes_win, "cursorline", false)
 
     -- Hashes window can be closed with just q in normal mode
     vim.keymap.set("n", "q", function()
@@ -47,11 +46,35 @@ end
 
 ---Triggers the hashes window title, does not appear until focused
 ---@param hashes_win integer Commits hashes window
-local function trigger_hashes_win_name_render(hashes_win)
-    local current_win = vim.api.nvim_get_current_win()
+---@param original_win integer Original window that is split off from
+local function trigger_hashes_win_name_render(hashes_win, original_win)
     vim.api.nvim_set_current_win(hashes_win)
-    vim.api.nvim_set_current_win(current_win)
+    vim.api.nvim_set_current_win(original_win)
 end
+
+---Scroll bind two windows together
+---@param hashes_win integer Commits hashes window
+---@param original_win integer Original window that is split off from
+local function scroll_bind_windows_together(hashes_win, original_win)
+    vim.api.nvim_win_set_option(original_win, "scrollbind", true)
+    vim.api.nvim_win_set_option(hashes_win, "scrollbind", true)
+end
+
+local function highlight_all_targeted_lines(lines) -- NOTE: emmylua needed
+    --[[ for _, id in ipairs(highlighted_matches) do
+        vim.fn.matchdelete(id)
+    end
+    highlighted_matches = {} ]]
+    -- NOTE: clear in separate function? return the ids for now?
+
+    for _, line_num in ipairs(lines) do
+        vim.fn.matchadd("CursorLine", "\\%" .. line_num .. "l")
+        -- local id = vim.fn.matchadd("CursorLine", "\\%" .. line_num .. "l")
+        -- table.insert(highlighted_matches, id)
+    end
+end
+
+-- NOTE: apply_to_both_windows? = so can do function (like highlihting) I want to do to both
 
 ---Displays all of a file's lines' commit hashes
 M.files_commit_hashes = function()
@@ -59,10 +82,25 @@ M.files_commit_hashes = function()
     local blame = blame_command.BlameCommand:new()
     local hashes = blame:commit_hashes_file(file_path)
 
+    local original_window = vim.api.nvim_get_current_win()
+
     local hashes_buf = generate_hashes_buf_with_text(hashes)
     local hashes_window = render_hashes_win(hashes_buf, #hashes)
     set_hashes_win_and_buf_properties(hashes_window, hashes_buf)
-    trigger_hashes_win_name_render(hashes_window)
+    trigger_hashes_win_name_render(hashes_window, original_window)
+    scroll_bind_windows_together(hashes_window, original_window)
+    -- NOTE: need to start accounting for moving the cursor next
+    -- just going off hash of first line for now
+    -- and turning off when hashes_win closes
+    local first_line_hash = hashes[1]
+    local line_numbers_with_hash = {}
+    for idx, hash in ipairs(hashes) do
+        if hash == first_line_hash then
+            table.insert(line_numbers_with_hash, idx)
+        end
+    end
+    local line_numbers = line_numbers_with_hash
+    highlight_all_targeted_lines(line_numbers)
 end
 
 return M
