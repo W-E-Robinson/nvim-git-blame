8ee7bd63da897ab39d7df79c2fbc15b98bcfba39
Will Robinson <william.ellis.robinson@gmail.com>
2026-02-10 22:35:25 +0000

    feat: highlights on one side due to initial cursor position, closing windows behaviours for hashes window
    highlighting moves on cursor move

--- lua/functionality/files_commit_hashes.lua
+++ lua/functionality/files_commit_hashes.lua
@@ -3,7 +3,7 @@ local blame_command = require("git_commands.blame_command")
 
 local M = {}
 
----Generates the hashes buf using the input table of commmit hashs
+---Generates the hashes buf using the input table of commt hashes
 ---@param hashes string[] hashes to generate the buf from
 ---@return integer
 local function generate_hashes_buf_with_text(hashes)
@@ -52,55 +52,109 @@ local function trigger_hashes_win_name_render(hashes_win, original_win)
     vim.api.nvim_set_current_win(original_win)
 end
 
----Scroll bind two windows together
+---Scoll binds two windows together for when moving
 ---@param hashes_win integer Commits hashes window
 ---@param original_win integer Original window that is split off from
-local function scroll_bind_windows_together(hashes_win, original_win)
+local function scroll_bind_two_windows_together(hashes_win, original_win)
     vim.api.nvim_win_set_option(original_win, "scrollbind", true)
     vim.api.nvim_win_set_option(hashes_win, "scrollbind", true)
 end
 
-local function highlight_all_targeted_lines(lines) -- NOTE: emmylua needed
-    --[[ for _, id in ipairs(highlighted_matches) do
-        vim.fn.matchdelete(id)
-    end
-    highlighted_matches = {} ]]
-    -- NOTE: clear in separate function? return the ids for now?
+---Returns the current row number of the cursor
+---@param window integer window to get current cursor row from
+---@return integer number row that the cursor is on
+local function current_row(window)
+    local cursor = vim.api.nvim_win_get_cursor(window)
+    local row = cursor[1]
+    return row
+end
 
-    for _, line_num in ipairs(lines) do
-        vim.fn.matchadd("CursorLine", "\\%" .. line_num .. "l")
-        -- local id = vim.fn.matchadd("CursorLine", "\\%" .. line_num .. "l")
-        -- table.insert(highlighted_matches, id)
+local namespace = vim.api.nvim_create_namespace("files_commit_hashes")
+
+---Clears the namespace of a buf and re highlights new lines
+---@param buf integer buf to have namespace cleared and rehighlighted
+---@param lines integer[] lines to now be highlighted
+local function clear_and_rehighlight_lines(buf, lines)
+    vim.api.nvim_buf_clear_namespace(buf, namespace, 0, -1)
+
+    for _, line in ipairs(lines) do
+        vim.api.nvim_buf_add_highlight(
+            buf,
+            namespace,
+            "CursorLine",
+            line - 1,
+            0,
+            -1
+        )
     end
 end
 
--- NOTE: apply_to_both_windows? = so can do function (like highlihting) I want to do to both
+---Updates the highlighting of the windows due to new cursor line
+---@param original_window integer original file window
+---@param hashes_window integer hashes window of commits
+---@param hashes string[] array of hashes for file lines
+local function update_highlights_both_windows(
+    original_window,
+    hashes_window,
+    hashes
+)
+    if
+        not vim.api.nvim_win_is_valid(original_window)
+        or not vim.api.nvim_win_is_valid(hashes_window)
+    then
+        return
+    end
+
+    local original_buf = vim.api.nvim_win_get_buf(original_window)
+    local hashes_buf = vim.api.nvim_win_get_buf(hashes_window)
+
+    local row = current_row(original_window)
+    local row_hash = hashes[row]
+
+    local same_hash_lines = {}
+    for idx, hash in ipairs(hashes) do
+        if hash == row_hash then
+            table.insert(same_hash_lines, idx)
+        end
+    end
+
+    clear_and_rehighlight_lines(original_buf, same_hash_lines)
+    clear_and_rehighlight_lines(hashes_buf, same_hash_lines)
+end
 
 ---Displays all of a file's lines' commit hashes
 M.files_commit_hashes = function()
+    -- Windows setup
+    local original_window = vim.api.nvim_get_current_win()
+
     local file_path = utils.current_file_path()
     local blame = blame_command.BlameCommand:new()
     local hashes = blame:commit_hashes_file(file_path)
-
-    local original_window = vim.api.nvim_get_current_win()
 
     local hashes_buf = generate_hashes_buf_with_text(hashes)
     local hashes_window = render_hashes_win(hashes_buf, #hashes)
     set_hashes_win_and_buf_properties(hashes_window, hashes_buf)
     trigger_hashes_win_name_render(hashes_window, original_window)
-    scroll_bind_windows_together(hashes_window, original_window)
-    -- NOTE: need to start accounting for moving the cursor next
-    -- just going off hash of first line for now
-    -- and turning off when hashes_win closes
-    local first_line_hash = hashes[1]
-    local line_numbers_with_hash = {}
-    for idx, hash in ipairs(hashes) do
-        if hash == first_line_hash then
-            table.insert(line_numbers_with_hash, idx)
-        end
-    end
-    local line_numbers = line_numbers_with_hash
-    highlight_all_targeted_lines(line_numbers)
+
+    scroll_bind_two_windows_together(hashes_window, original_window)
+
+    -- Highlighting based on line function called from
+    update_highlights_both_windows(original_window, hashes_window, hashes)
+
+    -- Autocommands
+    local group =
+        vim.api.nvim_create_augroup("FileCommitHashes", { clear = true })
+
+    vim.api.nvim_create_autocmd({ "CursorMoved" }, {
+        group = group,
+        callback = function()
+            update_highlights_both_windows(
+                original_window,
+                hashes_window,
+                hashes
+            )
+        end,
+    })
 end
 
 return M
